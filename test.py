deed9e14d814b34e8ba59b133693d9e0b5a2bfa4
ash78901 <ashd65059@gmail.com>
2025-11-10 18:56:02 -0500

    Add files via upload
    Calc

--- test.py
+++ test.py
@@ -1,4 +1,35 @@
-test_var = 4 + 5
+def calculator():
+    print("Welcome to the calculator.")
 
-# Print the value of test_var
-print(test_var)
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            operator = input("Choose an operator (+, -, *, /): ")
+            num2 = float(input("Enter the second number: "))
+
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                if num2 == 0:
+                    print("You tried to divide by zero. Invalid.")
+                    continue
+                result = num1 / num2
+            else:
+                print("Invalid operator. Try again.")
+                continue
+
+            print(f"The result is: {result}")
+
+        except ValueError:
+            print("That’s not a number. Try again.")
+
+        cont = input("Do you want to calculate something else? (yes/no): ").lower()
+        if cont != 'yes':
+            print("Goodbye.")
+            break
+
+calculator()
