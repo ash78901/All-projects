8c54be2b350a79004bf690dcb24cb829ecba647b
ash78901 <ashd65059@gmail.com>
2025-11-10 19:12:31 -0500

    Replace println with console.log for output

--- text.js
+++ text.js
@@ -7,13 +7,13 @@ function start() {
         for (var i = randomNumber; i < 1000000000000; i++){
             var inputNumber = readInt("What is your guess?");
             if (randomNumber > inputNumber){
-                println("Too low!");
+                console.log("Too low!");
 
             }else if(randomNumber < inputNumber){
-                println("Too high!");
+                console.log("Too high!");
 
             }else if(randomNumber == inputNumber){
-                println("Good job!");
+                console.log("Good job!");
                 break;
             }
         }
@@ -24,8 +24,8 @@ function start() {
 function randomNumberGame(){
    var guess = readInt("Quick! Pick a number between 1-7!");
    if (guess == 4){
-       println("You won the secret game :)");
+       console.log("You won the secret game :)");
    }else{
-       println("You lost the secret game :(");
+       console.log("You lost the secret game :(");
    }
 }
